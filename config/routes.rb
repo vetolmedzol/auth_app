59d206765abe4631ddb261ca2702853233bb8dbe
vitalii.mozol <vitalii.mozol@teladochealth.com>
2024-05-13 21:31:25 +0300

    added custom authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resource :session
+  resource :registration
+  resource :password
+  resource :password_reset
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'main#index'
 end
